0259c5a56d0c4a303d45a12740a7bdfa3c207a92
Don Gambas <ctmorris@gmail.com>
2022-02-12 16:05:52 -0800

    add cli commands for setting up auction

--- ts/tests/utils.ts
+++ ts/tests/utils.ts
@@ -1,7 +1,7 @@
 
 import {PublicKey } from '@solana/web3.js';
 
-export const AUCTION_PUBKEY = new PublicKey('BTE7AqJn4aG2MKZnaSTEgbQ4aCgPTDmphs5uxDnuDqvQ');
+export const AUCTION_PUBKEY = new PublicKey('CFik1YhMe1FLRe4koujoeEJVmNvfu9fQpyURdNgmKHHz');
 export const STORE_PUBKEY = new PublicKey('DNQzo4Aggw8PneX7BGY7niEkB8wfNJwx6DpV9BLBUUFF');
 export const TOKEN_MINT_PUBKEY = new PublicKey('1Qyhk9Pm1XktCN8RptQrwe9KnQLmiD7E58LpvqcnSV8');
 export const NFT_PUBKEY = new PublicKey('BTvURED6q7FeEP6Dmub19DgU611X9qJAMDaoHBKAdYLX');
